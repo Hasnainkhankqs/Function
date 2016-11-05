6d9919b9361f9879384549bb34ccbea4745277a0
Ali <anasbinbaba@hotmail.com>
2016-11-05 16:19:20 +0500

    adding return

--- app.js
+++ app.js
@@ -35,27 +35,36 @@
 // }
 // calculation(firstNumber,secondNumber,thirdNumber);
 
-var ingrd1 = ["egg","salt","oil"]
-var ingrd2 =["flour","salt","water"]
-var ingrd3 =["water","rice","zeera","oil"]
-var ingrd4 =[]
+// var ingrd1 = ["egg","salt","oil"]
+// var ingrd2 =["flour","salt","water"]
+// var ingrd3 =["water","rice","zeera","oil"]
+// var ingrd4 =[]
 
-function combine(ig1,ig2,ig3){
-    for(var i = 0 ; i < ig1.length ; i++){
-        if(ingrd4.indexOf(ig1[i]) == -1 ){
-            ingrd4.push(ig1[i]);
-        }
-    }
-        for(var i = 0 ; i < ig2.length ; i++){
-        if(ingrd4.indexOf(ig2[i]) == -1 ){
-            ingrd4.push(ig2[i]);
-        }
-    }
-        for(var i = 0 ; i < ig3.length ; i++){
-        if(ingrd4.indexOf(ig3[i]) == -1 ){
-            ingrd4.push(ig3[i]);
-        }
-    }
-    console.log(ingrd4);
+// function combine(ig1,ig2,ig3){
+//     for(var i = 0 ; i < ig1.length ; i++){
+//         if(ingrd4.indexOf(ig1[i]) == -1 ){
+//             ingrd4.push(ig1[i]);
+//         }
+//     }
+//         for(var i = 0 ; i < ig2.length ; i++){
+//         if(ingrd4.indexOf(ig2[i]) == -1 ){
+//             ingrd4.push(ig2[i]);
+//         }
+//     }
+//         for(var i = 0 ; i < ig3.length ; i++){
+//         if(ingrd4.indexOf(ig3[i]) == -1 ){
+//             ingrd4.push(ig3[i]);
+//         }
+//     }
+//     console.log(ingrd4);
+// }
+// combine(ingrd1,ingrd2,ingrd3);
+
+
+function squareNumber(number){
+    result = number*number;
+    // return "Hello sir";
+    return result;
 }
-combine(ingrd1,ingrd2,ingrd3);
+var squaredNumber = squareNumber(3);
+console.log(squaredNumber) ;
