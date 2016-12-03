30c9fd4fa425c24473f36e157686dd684bf06389
Ali <anasbinbaba@hotmail.com>
2016-12-03 14:03:54 +0500

    lab work

---
+++ 26-11-2016/app.js
@@ -0,0 +1,26 @@
+// var firstNumber = +prompt("write a number",1);
+// var secondNumber = +prompt("write a number",2);
+// var thirdNumber = +prompt("write a number",3);
+// var fourthNumber = +prompt("write a number",4);
+// var  fifthNumber = +prompt("write a number",5);
+// alert(firstNumber+secondNumber+thirdNumber+fourthNumber+fifthNumber);
+
+// all numbers will be sum
+
+// var writeNumbers = prompt("write some numbers");
+// var a = 0;
+// for(i=0; i<writeNumbers.length ;i++){
+//     a += +writeNumbers[i];
+// }
+// alert(a);
+
+
+// first and last numbers will be sum
+
+// var a = prompt("write a number");
+//     var b = +a[0];
+//     var c = parseInt(a.slice(a.length -1));
+// alert(b+c);
+
+var heading = document.getElementsByTagName("h1")[0];
+heading.getAttribute("class","ali");
