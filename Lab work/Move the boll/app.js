4d3c03f736a5f31463f9761b0da62bdf9b277ccf
Ali <anasbinbaba@hotmail.com>
2016-12-10 15:56:37 +0500

    top bottom working but unexpected

--- Lab work/Move the boll/app.js
+++ Lab work/Move the boll/app.js
@@ -1,30 +1,31 @@
 var ball = document.getElementById("ball");
 var left = 0;
 // var right = 0;
-var top =0;
-// var bottom = 0;
+// var top =0;
+var bottom = 0;
 var count = 0;
+var counttb =0;
 function moveLeft(){
     count-=5;
     left -= 5;
-    ball.style.marginLeft = left+"px";
+    ball.style.left = left+"px";
     console.log("left"+count)
 }
 function moveRight(){
    count+=5;
    left += 5;
-    ball.style.marginLeft = left+"px";
+    ball.style.left = left+"px";
     console.log("right"+count)
 }
 function moveTop(){
-    count-=5;
-    top -= 5;
-    ball.style.marginTop = top+"px";
-    console.log("top")
+    counttb-=5;
+    bottom += 5;
+    ball.style.bottom = bottom+"px";
+    console.log("top"+counttb)
 }
 function moveBottom(){
-    count+=5;
-    top += 5;
-    ball.style.marginTop = top+"px";
-    console.log("bottom")
+    counttb+=5;
+    bottom -= 5;
+    ball.style.bottom = bottom+"px";
+    console.log("bottom"+counttb)
 }
