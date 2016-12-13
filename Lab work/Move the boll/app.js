884bc07c19958a51edc226e9149403fb8b381e1c
Hasnain Khan <Hasnainkhankqs@gmail.com>
2016-12-13 20:11:51 +0500

    adding button controls

--- Lab work/Move the boll/app.js
+++ Lab work/Move the boll/app.js
@@ -1,7 +1,5 @@
 var ball = document.getElementById("ball");
 var left = 0;
-// var right = 0;
-// var top =0;
 var bottom = 0;
 var count = 0;
 var counttb =0;
@@ -18,14 +16,38 @@ function moveRight(){
     console.log("right"+count)
 }
 function moveTop(){
-    counttb-=5;
-    bottom += 5;
-    ball.style.bottom = bottom+"px";
+    counttb+=5;
+    bottom -= 5;
+    ball.style.top = bottom+"px";
     console.log("top"+counttb)
 }
 function moveBottom(){
-    counttb+=5;
-    bottom -= 5;
-    ball.style.bottom = bottom+"px";
+    counttb-=5;
+    bottom += 5;
+    ball.style.top = bottom+"px";
     console.log("bottom"+counttb)
 }
+
+
+
+
+document.addEventListener("keydown", function(key)
+{
+
+if(key.which == 98 || key.which == 40 )
+{
+    moveBottom()
+}
+else if(key.which == 100||key.which == 37)
+{
+    moveLeft()
+}
+else if(key.which == 102 || key.which == 39)
+{
+    moveRight()
+}
+else if(key.which == 104 || key.which == 38)
+{
+    moveTop()
+}
+});
